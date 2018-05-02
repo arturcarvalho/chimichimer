3040dadcf52e97a8c7799def33d7d2af3b3a6c05
Artur Carvalho <arturbelico@gmail.com>
2018-05-02 11:02:03 +0200

    make tool accept minutes for successive chimes

--- chimichimer.ps1
+++ chimichimer.ps1
@@ -1,39 +1,51 @@
-$host.ui.RawUI.WindowTitle = "chimi chimer"
-
 if ($args.length -eq 0) {
-    write-host "You need to put a parameter. E.g.: .\script.ps1 20"  -ForegroundColor Red;
+    write-host "You need to pass a parameter." -ForegroundColor Red;
+    write-host "E.g.: .\script.ps1 10+.5"  -ForegroundColor Red;
     exit;
 }
-
-$mins = $args[0];
 
 $sound = new-Object System.Media.SoundPlayer;
 $sound.SoundLocation = "c:\WINDOWS\Media\Windows Pop-up Blocked.wav";
 $sound.Play();
 
+
+write-host "Heard something? (ctrl+c to cancel)"  -ForegroundColor Yellow;
+
+###############################################################################
 #clear-host
-write-host "Heard something?"  -ForegroundColor Yellow;
+$firstArg = $args[0].ToString();
+$alarms = $firstArg.split("+") | ForEach-Object { 
+    $_.trim() / 1; # dividing string by 1 converts to float/int
+}; 
 
-write-host "Waiting $mins minutes (ctrl+c to cancel)"
-
-$now = get-date;
-$alarm = (get-date).AddMinutes($mins);
-
-while ($now -lt $alarm) {    
-    $now = get-date;
-    $newMinsRemaining = [Math]::ceiling(($alarm - $now).TotalMinutes);
-    if ($minsRemaining -ne $newMinsRemaining) {
-        $minsRemaining = $newMinsRemaining
-        $host.ui.RawUI.WindowTitle = "${minsRemaining}mins to go"
-        write-host "chi: $minsRemaining"
-    }    
+$alarmCount = $alarms.length
+for ($idx = 0; $idx -lt $alarmCount; $idx++) {
+    $idxDescription = $idx + 1;
+    $mins = $alarms[$idx];
+    $now = get-date;    
+    $alarmTime = (get-date).AddMinutes($mins);
     
-    start-sleep -seconds 1; # check every second
+    while ($now -lt $alarmTime) {    
+        $now = get-date;
+        $newMinsRemaining = [Math]::ceiling(($alarmTime - $now).TotalMinutes);
+        if ($minsRemaining -ne $newMinsRemaining) {
+            $minsRemaining = $newMinsRemaining
+            $msgTimeRemaining = "${minsRemaining}mins to go ";
+            $msgAlarms = "(${idxDescription}/${alarmCount})";
+            $host.ui.RawUI.WindowTitle = $msgTimeRemaining + $msgAlarms;
+            write-host $msgTimeRemaining -NoNewline -ForegroundColor Green
+            write-host $msgAlarms 
+        }    
+        
+        start-sleep -seconds 1; # check every second
+    }
+
+    # time has passed, so play the audio cue
+    $sound.SoundLocation = "c:\WINDOWS\Media\Windows Unlock.wav";
+    $sound.Play();
+    write-host "Alarm $idxDescription done";
 }
 
-
-# time has passed, so play the audio queue
-$sound.SoundLocation = "c:\WINDOWS\Media\Windows Unlock.wav";
-$sound.Play();
-
-write-host "Finished."
+$noAlarmsMsg = "No more alarms.";
+$host.ui.RawUI.WindowTitle = $noAlarmsMsg;
+write-host $noAlarmsMsg
