2e59c02f80cab523db2f84a766d52a628fc784b2
Artur Carvalho <arturbelico@gmail.com>
2018-05-02 11:16:30 +0200

    implement enter several alarms with multiplier

--- chimichimer.ps1
+++ chimichimer.ps1
@@ -1,6 +1,9 @@
+# tested only on win 10 with ConEmu.
+
 if ($args.length -eq 0) {
     write-host "You need to pass a parameter." -ForegroundColor Red;
     write-host "E.g.: .\script.ps1 10+.5"  -ForegroundColor Red;
+    write-host "E.g.: .\script.ps1 10*3 (mins on left, mult on right. + doesnt work here)"  -ForegroundColor Red;
     exit;
 }
 
@@ -8,16 +11,26 @@ $sound = new-Object System.Media.SoundPlayer;
 $sound.SoundLocation = "c:\WINDOWS\Media\Windows Pop-up Blocked.wav";
 $sound.Play();
 
-
-write-host "Heard something? (ctrl+c to cancel)"  -ForegroundColor Yellow;
+write-host "Heard something? If not. check volume. (ctrl+c to cancel)"  -ForegroundColor Yellow;
 
 ###############################################################################
 #clear-host
-$firstArg = $args[0].ToString();
-$alarms = $firstArg.split("+") | ForEach-Object { 
-    $_.trim() / 1; # dividing string by 1 converts to float/int
-}; 
 
+$firstArg = $args[0].ToString();
+
+if ($firstArg -match "\*"){ ## if it has multiplication, it won't have sum.
+    $arr = $firstArg.split("*");
+    $m = $arr[0];
+    $howManyTimes =$arr[1];
+    $alarms = 1..$howManyTimes | ForEach-Object {
+        $m.trim() / 1; # dividing string by 1 converts to float/int # XXX dupe code
+    }
+} else {
+    $alarms = $firstArg.split("+") | ForEach-Object { 
+        $_.trim() / 1; # dividing string by 1 converts to float/int
+    };     
+}
+    
 $alarmCount = $alarms.length
 for ($idx = 0; $idx -lt $alarmCount; $idx++) {
     $idxDescription = $idx + 1;
